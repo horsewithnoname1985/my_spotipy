b66d5a80290ab0152efc7dc833b6c53c3f53e1b7
horsewithnoname1985 <Kz8zcuFXvjxm5]iN=Kf;c}hc>
2020-01-29 15:51:25 +0100

    added project files

--- spotipy_first_try.py
+++ spotipy_first_try.py
@@ -1,32 +1,45 @@
 import spotipy
+import random
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_credentials_manager = SpotifyClientCredentials()
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-# https://open.spotify.com/user/11125633573?si=M0E30eJdQUS4qqVn8iXHzw
-# https://open.spotify.com/playlist/08m8krYpkRJko7KHLH9Fpv?si=o84GfeGbTryUSCy-UVzaLg
-user_id = "M0E30eJdQUS4qqVn8iXHzw"
-playlist_id = "08m8krYpkRJko7KHLH9Fpv?si=o84GfeGbTryUSCy-UVzaLg"
 
-tracks = sp.user_playlist_tracks(user_id, playlist_id)['tracks']
+def get_random_genre(lang):
+    if lang == "czech":
+        genres = ["Rock", "Pop", "House", "Hip-Hop", "Klasická", "R&B", "Džez",
+                  "Metal"]
 
-for i, track in enumerate(tracks['items']):
-    print("%s;%s;%s;%s" %
-          (track['track']['artists'][0]['name'],
-           track['track']['name'],
-           track['track']['album']['name'],
-           # TODO: genre not available - add random genre creator
-           track['track']['album']['release_date'].split("-")[0]
-           )
+    if lang == "thai":
+        genres = ["ร็อค", "ป๊อบ", "บ้าน", "ฮิบ-ฮอบ", "คฺลาด-สิก",
+                  "อาร์แอนด์บี", "แจ็ส", "Metal"]
+    else:
+        genres = ["Rock", "Pop", "House", "Soundtrack", "Hip-Hop", "Classic",
+                  "R&B", "Jazz", "Metal"]
 
-          )
+    return random.choice(genres)
 
-# playlists = sp.user_playlists('spotify')
-# while playlists:
-#     for i, playlist in enumerate(playlists['items']):
-#         print("%4d %s %s" %
-#               (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
-#     if playlists['next']:
-#         playlists = sp.next(playlists)
-#     else:
-#         playlists = None
+
+if __name__ == '__main__':
+
+    client_credentials_manager = SpotifyClientCredentials()
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    # https://open.spotify.com/user/11125633573?si=M0E30eJdQUS4qqVn8iXHzw
+    # https://open.spotify.com/playlist/08m8krYpkRJko7KHLH9Fpv?si=o84GfeGbTryUSCy-UVzaLg
+    # https://open.spotify.com/playlist/70ANdI7a9xTCTICgpvOkL8?si=H83IS22IS3SA_83hO4hnqA
+    # https://open.spotify.com/playlist/70ANdI7a9xTCTICgpvOkL8?si=7T-a0rVQTE-Lv91HWWa3ew
+    # https://open.spotify.com/playlist/0zFJYziDwEAVmGLqvMbmde?si=R0TT76nAQEmwi-VCnIYeHQ
+    # https://open.spotify.com/playlist/3F1ZgOApK9ngsemuDzZrH0?si=4FUArpuaRnSBghXVGGZwcw
+    user_id = "M0E30eJdQUS4qqVn8iXHzw"
+    playlist_id = "2l3CU7QId7ckcLzR2GFjTA?si=jxTjlgwhQtywsdAQHRXSgg"
+
+    tracks = sp.user_playlist_tracks(user_id, playlist_id, limit=100)['tracks']
+
+    for i, track in enumerate(tracks['items']):
+        print("%s;%s;%s;%s;%s;%s" %
+              (track['track']['artists'][0]['name'],
+               track['track']['name'],
+               track['track']['album']['name'],
+               track['track']['artists'][0]['name'],
+               get_random_genre("polish"),
+               track['track']['album']['release_date'].split("-")[0]
+               )
+              )
